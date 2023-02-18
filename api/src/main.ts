467623f9eeac299876987408f4ced1068861be2f
sarunas <sarunas.palianskas@ktu.edu>
2023-02-18 17:14:16 +0200

    api: enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api');
+  app.enableCors({
+    origin: '*',
+  });
 
   await app.listen(3000);
 }
