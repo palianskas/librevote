7f4b8b6c097688fa8eca3aaea015e5b4ff3f5fa4
sarunas <sarunas.palianskas@ktu.edu>
2023-03-05 13:52:18 +0200

    api: remove operational context use

--- api/src/modules/campaigns.module/routes/campaigns.controller.ts
+++ api/src/modules/campaigns.module/routes/campaigns.controller.ts
@@ -6,10 +6,11 @@ import {
   NotFoundException,
   Param,
   Post,
+  Request,
 } from '@nestjs/common';
+import { IAuthenticatedRequest } from 'src/modules/auth.module/routes/models/auth-contracts.model';
 import { CampaignsService } from 'src/modules/campaigns.module/campaigns.service';
 import { CampaignDto } from 'src/modules/campaigns.module/models/campaign.dto';
-import { OperationalContextService } from 'src/modules/core.module/operational-context/operational-context.service';
 import { CampaignCreateHandler } from './handlers/campaign-create.handler';
 import { CampaignSearchHandler } from './handlers/campaign-search.handler';
 import {
@@ -25,18 +26,20 @@ export class CampaignsController {
     private readonly campaignService: CampaignsService,
     private readonly campaignSearchHandler: CampaignSearchHandler,
     private readonly campaignCreateHandler: CampaignCreateHandler,
-    private readonly operationalContextService: OperationalContextService,
   ) {}
 
   @Get(':id')
-  async get(@Param('id') id: string): Promise<CampaignDto> {
+  async get(
+    @Param('id') id: string,
+    @Request() request: IAuthenticatedRequest,
+  ): Promise<CampaignDto> {
     const campaign = await this.campaignService.get(id);
 
     if (!campaign) {
       throw new NotFoundException();
     }
 
-    const user = this.operationalContextService.context.user;
+    const user = request.user;
 
     if (this.campaignService.isAccessDeniedToCampaign(user, campaign)) {
       throw new ForbiddenException();
@@ -49,8 +52,9 @@ export class CampaignsController {
   @Post()
   async create(
     @Body() body: ICampaignCreateRequest,
+    @Request() request: IAuthenticatedRequest,
   ): Promise<ICampaignCreateResponse> {
-    const user = this.operationalContextService.context.user;
+    const user = request.user;
 
     const response = await this.campaignCreateHandler.handle(user, body);
 
@@ -60,8 +64,9 @@ export class CampaignsController {
   @Post('search')
   async search(
     @Body() body: ICampaignSearchRequest,
+    @Request() request: IAuthenticatedRequest,
   ): Promise<ICampaignSearchResponse> {
-    const user = this.operationalContextService.context.user;
+    const user = request.user;
 
     const response = await this.campaignSearchHandler.handle(body);
 
