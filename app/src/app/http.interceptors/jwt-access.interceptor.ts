a955091c10622aff25a4b40bb2a7a6064b83aac9
sarunas <sarunas.palianskas@ktu.edu>
2023-03-17 16:16:40 +0200

    app: add request retry on access token refresh

--- app/src/app/http.interceptors/jwt-access.interceptor.ts
+++ app/src/app/http.interceptors/jwt-access.interceptor.ts
@@ -7,7 +7,7 @@ import {
   HttpErrorResponse,
 } from '@angular/common/http';
 
-import { Observable, catchError, EMPTY } from 'rxjs';
+import { Observable, catchError, from, switchMap } from 'rxjs';
 import { ConfigService } from '../common.module/services/config.service';
 import { AuthError } from '../auth.module/models/auth-error.enum';
 import { JwtService } from '../auth.module/services/jwt.service';
@@ -26,26 +26,47 @@ export class JwtAccessInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const accessToken = localStorage.getItem(
-      this.configService.ACCESS_TOKEN_LOCAL_STORAGE_KEY
-    );
-
-    req = req.clone({
-      setHeaders: {
-        'Content-Type': 'application/json; charset=utf-8',
-        Accept: 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+    req = this.withAuthHeaders(this.withContentHeaders(req));
 
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.error?.message === AuthError.AccessTokenExpired) {
-          this.jwtService.refreshAccess();
+          return from(this.jwtService.refreshAccess()).pipe(
+            switchMap(() => {
+              req = this.withAuthHeaders(req);
+
+              return next.handle(req);
+            })
+          );
         }
 
         throw error;
       })
     );
+  }
+
+  private withContentHeaders<T>(request: HttpRequest<T>): HttpRequest<T> {
+    const authenticatedRequest = request.clone({
+      setHeaders: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+      },
+    });
+
+    return authenticatedRequest;
+  }
+
+  private withAuthHeaders<T>(request: HttpRequest<T>): HttpRequest<T> {
+    const accessToken = localStorage.getItem(
+      this.configService.ACCESS_TOKEN_LOCAL_STORAGE_KEY
+    );
+
+    const authenticatedRequest = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return authenticatedRequest;
   }
 }
