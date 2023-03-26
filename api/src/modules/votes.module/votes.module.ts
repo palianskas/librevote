7a87de08bdfcc1ab4bc18180d2e5496511bdce46
sarunas <sarunas.palianskas@ktu.edu>
2023-03-26 17:08:29 +0300

    api: add missing declarations to votes module

--- api/src/modules/votes.module/votes.module.ts
+++ api/src/modules/votes.module/votes.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CampaignsModule } from '../campaigns.module/campaigns.module';
+import { DataModule } from '../data.module/data.module';
 import { VoteCastHandler } from './routes/handlers/vote-cast.handler';
 import { VotesController } from './routes/votes.controller';
 import { VouchersController } from './routes/vouchers.controller';
+import { VotesRepository } from './votes.repository';
+import { VotesService } from './votes.service';
 
 @Module({
-  imports: [CampaignsModule],
+  imports: [CampaignsModule, DataModule],
   controllers: [VotesController, VouchersController],
-  providers: [VoteCastHandler],
+  providers: [VotesService, VotesRepository, VoteCastHandler],
 })
 export class VotesModule {}
