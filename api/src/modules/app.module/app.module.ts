4e8058f2e72283516b2de18e2bd6edd59f08f42c
sarunas <sarunas.palianskas@ktu.edu>
2023-03-26 10:54:56 +0300

    api: remove OperationalContext usage

--- api/src/modules/app.module/app.module.ts
+++ api/src/modules/app.module/app.module.ts
@@ -1,12 +1,10 @@
-import { Module, Scope } from '@nestjs/common';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { StatusController } from 'src/modules/core.module/routes/status/status.controller';
 import { AuthModule } from '../auth.module/auth.module';
 import { JwtAuthGuard } from '../auth.module/guards/jwt-auth.guard';
 import { CampaignsModule } from '../campaigns.module/campaigns.module';
 import { CoreModule } from '../core.module/core.module';
-import { OperationalContextInterceptor } from '../core.module/operational-context/operational-context.interceptor';
-import { OperationalContextService } from '../core.module/operational-context/operational-context.service';
 import { StatusService } from '../core.module/status/status.service';
 import { DataModule } from '../data.module/data.module';
 import { UsersModule } from '../users.module/users.module';
@@ -16,15 +14,9 @@ import { UsersModule } from '../users.module/users.module';
   controllers: [StatusController],
   providers: [
     StatusService,
-    OperationalContextService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      scope: Scope.REQUEST,
-      useClass: OperationalContextInterceptor,
     },
   ],
 })
--- api/src/modules/core.module/core.module.ts
+++ api/src/modules/core.module/core.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StatusController } from 'src/modules/core.module/routes/status/status.controller';
-import { OperationalContextService } from './operational-context/operational-context.service';
 import { StatusService } from './status/status.service';
 
 @Module({
   controllers: [StatusController],
-  providers: [StatusService, OperationalContextService],
-  exports: [OperationalContextService],
+  providers: [StatusService],
 })
 export class CoreModule {}
