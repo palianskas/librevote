7db6e8c78a8ab8e26d3b23a6c665611e83551a8e
sarunas <sarunas.palianskas@ktu.edu>
2023-03-31 17:58:23 +0300

    api: fix OptionalJwtAuthGuard

--- api/src/modules/auth.module/guards/optional-jwt-auth.guard.ts
+++ api/src/modules/auth.module/guards/optional-jwt-auth.guard.ts
@@ -3,7 +3,11 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
-  handleRequest<User>(user: User | null): User | null {
+  handleRequest<User>(err: any, user: User, info: Error): User | null {
+    if (!user) {
+      return null;
+    }
+
     return user;
   }
 }
