e73c27d1b087d80c83123ff805e13b63f10663e9
sarunas <sarunas.palianskas@ktu.edu>
2023-03-31 17:04:09 +0300

    api: add VotingVoucherDto

--- api/src/modules/votes.module/models/voting-voucher.model.ts
+++ api/src/modules/votes.module/models/voting-voucher.model.ts
@@ -1,7 +1,37 @@
 import { User, VotingVoucher as VotingVoucherEntity } from '@prisma/client';
 import { Campaign } from 'src/modules/campaigns.module/models/campaign/campaign.model';
+import { UserDto } from 'src/modules/users.module/models/user.dto';
 
 export type VotingVoucher = VotingVoucherEntity & {
   campaign: Campaign;
-  designatedUser?: User;
+  designatedUser: User | null;
 };
+
+export class VotingVoucherDto {
+  id?: string;
+  campaignId: string;
+  issueDate: Date;
+  validUntilDate?: Date;
+  designatedUserId?: string;
+  designatedUser?: UserDto;
+
+  static map(entity: VotingVoucher): VotingVoucherDto {
+    const dto = new VotingVoucherDto();
+
+    dto.id = entity.id;
+    dto.campaignId = entity.campaignId;
+    dto.issueDate = entity.issueDate;
+
+    if (!!entity.validUntilDate) {
+      dto.validUntilDate = new Date(entity.validUntilDate);
+    }
+    if (!!entity.designatedUserId) {
+      dto.designatedUserId = entity.designatedUserId;
+    }
+    if (!!entity.designatedUser) {
+      dto.designatedUser = UserDto.map(entity.designatedUser);
+    }
+
+    return dto;
+  }
+}
