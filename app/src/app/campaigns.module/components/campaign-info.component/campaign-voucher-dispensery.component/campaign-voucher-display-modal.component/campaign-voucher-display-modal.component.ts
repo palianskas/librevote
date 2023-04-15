fe83479a58a80069b8719d8c4a1799d7aada1779
sarunas <sarunas.palianskas@ktu.edu>
2023-04-15 10:29:15 +0300

    app: add valid-until-date to voucher display modal

--- app/src/app/campaigns.module/components/campaign-info.component/campaign-voucher-dispensery.component/campaign-voucher-display-modal.component/campaign-voucher-display-modal.component.ts
+++ app/src/app/campaigns.module/components/campaign-info.component/campaign-voucher-dispensery.component/campaign-voucher-display-modal.component/campaign-voucher-display-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { VoucherContainer } from 'src/app/votes.module/models/voucher-container.model';
 import * as QRcode from 'qrcode';
+import { StringHelpers } from 'src/app/encryption.module/string.helpers';
 
 @Component({
   selector: 'app-campaign-voucher-display-modal',
@@ -26,6 +27,10 @@ export class CampaignVoucherDisplayModalComponent {
       'voucher-display-modal-close-button'
     );
     button.click();
+  }
+
+  formatDate(date: Date): string {
+    return StringHelpers.dateTimeToString(date);
   }
 
   private generateVoucherQrCode(): void {
