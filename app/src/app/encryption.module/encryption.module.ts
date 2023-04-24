534a28079ea93f92727eb6c088e8812ea23a0a6f
sarunas <sarunas.palianskas@ktu.edu>
2023-04-24 20:59:24 +0300

    app: add service declarations to EncryptionModule

--- app/src/app/encryption.module/encryption.module.ts
+++ app/src/app/encryption.module/encryption.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EncryptionDomainFactory } from './encryption-domain/encryption-domain.factory';
+import { EncryptionService } from './services/encryption.service';
+import { KeyContainerService } from './key-container/key-container.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  providers: [EncryptionDomainFactory, EncryptionService, KeyContainerService],
 })
 export class EncryptionModule {}
